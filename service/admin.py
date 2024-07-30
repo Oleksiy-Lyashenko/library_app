f7847ccc1f9689aaf38dc2112404619199fc7443
Oleksii <oleksij.lyashenko@gmail.com>
2024-07-30 16:45:18 +0300

    Add models - Book, Payment, Borrowing"

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from service.models import Book, Payment, Borrowing
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Payment)
+admin.site.register(Borrowing)
+
--- service/models.py
+++ service/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.validators import MinValueValidator, URLValidator
 
-# Create your models here.
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = 'HARD', 'Hard Cover'
+        SOFT = 'SOFT', 'Soft Cover'
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=Cover.choices)
+    inventory = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="reservations", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.user.email} borrowed {self.book.title}"
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        PAID = 'PAID', 'Paid'
+
+    class Type(models.TextChoices):
+        PAYMENT = 'PAYMENT', 'Payment'
+        FINE = 'FINE', 'Fine'
+
+    status = models.CharField(max_length=7, choices=Status.choices, default=Status.PENDING)
+    type = models.CharField(max_length=7, choices=Type.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField(validators=[URLValidator()])
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.type} - {self.status} - ${self.money_to_pay}"
